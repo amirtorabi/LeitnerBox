920416487befce77bd70c75a72de192dd78fc699
Amir <dfd.torabi@gmail.com>
2019-04-17 00:30:32 +0430

    started Card activity

---
+++ app/src/main/java/com/example/leitnerbox/activity/ProgressBarActivity.java
@@ -0,0 +1,15 @@
+package com.example.leitnerbox.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.leitnerbox.R;
+
+public class ProgressBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_bar);
+    }
+}
