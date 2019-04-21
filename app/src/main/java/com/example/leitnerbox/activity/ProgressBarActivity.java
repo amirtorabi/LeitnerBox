50ad6cb2ac1a36e0d2f8c03f308f89afe74044fa
amirtorabi <dfd.torabi@gmail.com>
2019-04-21 21:15:50 +0430

    Add Card Animation

--- app/src/main/java/com/example/leitnerbox/activity/ProgressBarActivity.java
+++ app/src/main/java/com/example/leitnerbox/activity/ProgressBarActivity.java
@@ -7,29 +7,34 @@ import android.support.v7.widget.CardView;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.daimajia.numberprogressbar.NumberProgressBar;
 import com.daimajia.numberprogressbar.OnProgressBarListener;
 import com.example.leitnerbox.R;
+import com.wajahatkarim3.easyflipview.EasyFlipView;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class ProgressBarActivity extends AppCompatActivity {
 
     NumberProgressBar numberProgressBar;
-    CardView myCardView;
-    private Animation animation;
-
+    CardView cardFront, cardBack;
+    private EasyFlipView easyFlipView;
+    private boolean isFrontShow = false;
+    private Button btnRight, btnWrong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_bar);
 
+        initViews();
         setupNumberProgressBar();
 
-         animation = AnimationUtils.loadAnimation(this, R.anim.swing_up_left);
+
+        easyFlipView = (EasyFlipView) findViewById(R.id.easyFlipView);
 
 
         animateCard();
@@ -79,15 +84,45 @@ public class ProgressBarActivity extends AppCompatActivity {
 
     public void animateCard() {
 
-        myCardView = findViewById(R.id.myCardView);
+        cardFront = findViewById(R.id.cardFront);
+        cardBack = findViewById(R.id.cardBack);
 
-        myCardView.setOnClickListener(new View.OnClickListener() {
+        cardFront.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myCardView.startAnimation(animation);
+
+                easyFlipView.flipTheView();
+
+                btnRight.setVisibility(View.VISIBLE);
+                btnWrong.setVisibility(View.VISIBLE);
+            }
+        });
+
+        cardBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                easyFlipView.flipTheView();
+
+                btnRight.setVisibility(View.INVISIBLE);
+                btnWrong.setVisibility(View.INVISIBLE);
+
+
 
             }
         });
+
+    }
+
+    private void initViews() {
+
+        btnRight = findViewById(R.id.btnRight);
+        btnWrong = findViewById(R.id.btnWrong);
+
+        btnRight.setVisibility(View.INVISIBLE);
+        btnWrong.setVisibility(View.INVISIBLE);
+
+
 
     }
 
