d40e59a301c2746f09dccf0d55e617577eef62db
amirtorabi <dfd.torabi@gmail.com>
2019-05-02 10:43:37 +0430

    Solving some font issues

--- app/src/main/java/com/example/leitnerbox/adapter/AdapterBox.java
+++ app/src/main/java/com/example/leitnerbox/adapter/AdapterBox.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import com.example.leitnerbox.R;
 import com.example.leitnerbox.model.Box;
+import com.example.leitnerbox.utils.FaNum;
 
 import java.util.List;
 
@@ -35,8 +36,8 @@ public class AdapterBox extends RecyclerView.Adapter<AdapterBox.OriginalViewHold
 
         Box box = boxList.get(i);
 
-        holder.tvBoxTitle.setText(box.getBoxNumber());
-        holder.tvBoxCartNumber.setText(box.getBoxTotalCards());
+        holder.tvBoxTitle.setText(FaNum.convert2PerisanNumber(box.getBoxNumber()));
+        holder.tvBoxCartNumber.setText(FaNum.convert2PerisanNumber(box.getBoxTotalCards()));
 
 
     }
---
+++ app/src/main/java/com/example/leitnerbox/utils/FaNum.java
@@ -0,0 +1,50 @@
+package com.example.leitnerbox.utils;
+
+public class FaNum {
+
+    private static String[] persianNumbers = new String[]{ "۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹" };
+
+    public static String convert(String faNumbers) {
+        String[][] mChars = new String[][]{
+                {"0", "۰"},
+                {"1", "۱"},
+                {"2", "۲"},
+                {"3", "۳"},
+                {"4", "۴"},
+                {"5", "۵"},
+                {"6", "۶"},
+                {"7", "۷"},
+                {"8", "۸"},
+                {"9", "۹"}
+        };
+
+        for (String[] num : mChars) {
+
+            faNumbers = faNumbers.replace(num[0], num[1]);
+
+        }
+
+        return faNumbers;
+    }
+
+    public static String convert2PerisanNumber(String text) {
+        if (text.length() == 0) {
+            return "";
+        }
+        String out = "";
+        int length = text.length();
+        for (int i = 0; i < length; i++) {
+            char c = text.charAt(i);
+            if ('0' <= c && c <= '9') {
+                int number = Integer.parseInt(String.valueOf(c));
+                out += persianNumbers[number];
+            } else if (c == '٫') {
+                out += '،';
+            } else {
+                out += c;
+            }
+        }
+        return out;
+    }
+
+}
