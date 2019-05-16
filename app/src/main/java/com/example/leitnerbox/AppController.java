1b6e9aa56fe399203cf521c7b5d2ca387dd9f7d1
amirtorabi <dfd.torabi@gmail.com>
2019-05-16 08:51:29 +0430

    some changes on AppController

--- app/src/main/java/com/example/leitnerbox/AppController.java
+++ app/src/main/java/com/example/leitnerbox/AppController.java
@@ -1,13 +1,21 @@
 package com.example.leitnerbox;
 
 import android.app.Application;
+
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
-public class FontIranSans extends Application {
+public class AppController extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        setupDefaultFont();
+
+
+
+    }
+
+    private void setupDefaultFont() {
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/iran_sans_regular.ttf")
                 .setFontAttrId(R.attr.fontPath)
